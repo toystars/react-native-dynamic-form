3f9adc9ff5df28f286f309ddc2beee6801163a6b
Mustapha Babatunde <toystars2008@gmail.com>
2018-05-22 08:13:25 +0100

    Export theme config data

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// main package
 import DynamicForm from './lib/DynamicForm';
+
+// theming config
+import {
+  defaultColors as colors,
+  defaultFonts as fonts,
+  defaultTheme as theme,
+  buildTheme as builder,
+} from './config/styles';
+
+export const defaultColors = colors;
+export const defaultFonts = fonts;
+export const defaultTheme = theme;
+export const buildTheme = builder;
 
 export default DynamicForm;
